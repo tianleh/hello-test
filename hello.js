e5d0caec95b390bb629a22ba718588c840c13f11
Tianle Huang <tianleh@amazon.com>
2021-08-26 17:58:43 -0700

    import util function by file import

---
+++ hello.js
@@ -0,0 +1,4 @@
+// export default function hello(message) {
+
+//     console.log('Message is ' + message)
+// }
---
+++ index.js
@@ -0,0 +1,14 @@
+import {hello} from 'test-library'
+// import * as hello from 'test-library'
+// import { hello } from './hello'
+
+
+
+
+// import * as Utils from 'hello.js';
+
+// import hello from './hello.js'
+
+// console.log(Utils)
+
+hello('today')
